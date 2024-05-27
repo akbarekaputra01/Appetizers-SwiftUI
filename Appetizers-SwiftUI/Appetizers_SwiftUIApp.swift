b3176c69e61c2d74f5c056b7b0bc56799c45932f
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-05-27 20:31:26 +0700

    Initial Commit

---
+++ Appetizers-SwiftUI/Appetizers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_SwiftUIApp.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Akbar Eka Putra on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
