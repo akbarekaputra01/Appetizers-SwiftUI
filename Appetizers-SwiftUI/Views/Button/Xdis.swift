10874f18dfd4d4f4fefd85de56caf6f7d2ca1396
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-06 14:23:15 +0700

    feat: add appetizer detail view

---
+++ Appetizers-SwiftUI/Views/Button/Xdis.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Akbar Eka Putra on 06/06/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
